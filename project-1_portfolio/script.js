3bc8efa3eb1cc91f5c743f468f9fed19c22a3c64
saifcode19 <saif.java18@gmail.com>
2024-11-07 12:45:13 +0530

    Add files via upload

--- project-1_portfolio/script.js
+++ project-1_portfolio/script.js
@@ -1,3 +1,11 @@
+/*===========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 
 let sections = document.querySelectorAll('section');
@@ -7,19 +15,52 @@ window.onscroll = () => {
     
     sections.forEach(sec => {
     
-        let top = window.scrollY; // Move this outside the loop to optimize performance
-        let offset = sec.offsetTop - 150; // Adjust the offset if needed
-        let height = sec.offsetHeight; // Corrected spelling error
+        let top = window.scrollY; 
+        let offset = sec.offsetTop - 150; 
+        let height = sec.offsetHeight; 
         let id = sec.getAttribute('id');
 
-        // Check if the section is in view
+        
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active'); // Remove active class from all links
-           
-            // Add active class to the link that corresponds to the section in view
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+            navLinks.forEach(links => {
+                links.classList.remove('active'); 
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
         });
         };
     });
+
+    /* ======================sticky nav==========*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    /*==========================*/
+    
+    menuIcon.classList.remove('box-x');
+    navbar.classList.remove('active');
+
 };
+
+/*==============*/
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{ origin: 'top'});
+ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form',{origin:
+     'bottom'});
+     ScrollReveal().reveal('.home-content h1, .about',{ origin: 'left'});
+     ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right'});
+
+     /*==========*/
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Web Developer', 'Engineer', 'Creator'],
+    typeSpee : 500,
+    backspeed: 500,
+    backDelay: 2000,
+    loop: true
+});
